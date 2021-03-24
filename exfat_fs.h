0b2360c8e19c9ca9b4f51ce61241cf569b9516a1
Namjae Jeon <namjae.jeon@samsung.com>
2021-03-24 18:30:08 +0900

    Revert "exfat: add support ioctl and FITRIM function"
    This reverts commit 89965a97eb6c1335bbe44095c35f3124b0e84861.

--- exfat_fs.h
+++ exfat_fs.h
@@ -425,7 +425,6 @@ int exfat_set_bitmap(struct inode *inode, unsigned int clu, bool sync);
 void exfat_clear_bitmap(struct inode *inode, unsigned int clu, bool sync);
 unsigned int exfat_find_free_bitmap(struct super_block *sb, unsigned int clu);
 int exfat_count_used_clusters(struct super_block *sb, unsigned int *ret_count);
-int exfat_trim_fs(struct inode *inode, struct fstrim_range *range);
 
 /* file.c */
 extern const struct file_operations exfat_file_operations;
@@ -449,9 +448,6 @@ int exfat_getattr(struct vfsmount *mnt, struct dentry *dentry,
 #endif
 #endif
 int exfat_file_fsync(struct file *file, loff_t start, loff_t end, int datasync);
-long exfat_ioctl(struct file *filp, unsigned int cmd, unsigned long arg);
-long exfat_compat_ioctl(struct file *filp, unsigned int cmd,
-				unsigned long arg);
 
 
 /* namei.c */
